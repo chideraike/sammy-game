d0623561e7c68de2816bb1efc3a5da32ba3c0bff
Chidera <chideraike.s@gmail.com>
2024-08-25 13:53:14 +0100

    :tada: Completed tutorial

---
+++ src/save.js
@@ -0,0 +1,25 @@
+import { BaseDirectory, readTextFile, writeTextFile } from "@tauri-apps/api/fs"
+
+function makeSaveSystem(savefileName) {
+  return {
+    data: {},
+    async save() {
+      await writeTextFile(savefileName, JSON.stringify(this.data), {
+        dir: BaseDirectory.AppLocalData,
+      })
+    },
+    async load() {
+      try {
+        this.data = JSON.parse(
+          await readTextFile(savefileName, {
+            dir: BaseDirectory.AppLocalData,
+          })
+        )
+      } catch {
+        this.data = {}
+      }
+    },
+  }
+}
+
+export const saveSystem = makeSaveSystem("save.json")
